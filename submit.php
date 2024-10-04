ac88c659a50d6c30d975b1c62dbc337c252e31c4
aminbiography <biographyamin@gmail.com>
2024-10-05 00:06:23 +0600

    Add existing project files to Git

--- submit.php
+++ submit.php
@@ -1,5 +1,7 @@
 <?php
+// Check if the form has been submitted using the POST method
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve the form data from the POST request
     $firstName = $_POST['firstName'];
     $lastName = $_POST['lastName'];
     $nicName = $_POST['nicName'];
@@ -11,8 +13,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
     $phone = $_POST['phone'];
 
+    // Specify the recipient email address
     $to = "browsing.amin@gmail.com";
+
+    // Set the subject line for the email
     $subject = "Registration Form Submission";
+
+    // Compose the email message with the submitted form data
     $message = "
     First Name: $firstName\n
     Last Name: $lastName\n
@@ -26,11 +33,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     Phone/WHATSAPP: $phone
     ";
 
+    // Set the email headers, including the sender's email
     $headers = "From: $email";
 
+    // Attempt to send the email and check if it was successful
     if (mail($to, $subject, $message, $headers)) {
+        // If the email was sent successfully, display a success message
         echo "Thank you for registering. Your details have been sent.";
     } else {
+        // If there was an error sending the email, display an error message
         echo "Sorry, there was an issue sending your data. Please try again.";
     }
 }
